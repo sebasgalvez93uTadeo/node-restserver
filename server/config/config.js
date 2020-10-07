0916127feb18c6f14fc4a1e4e61e549a300538f2
sebasgalvez-loro <s.galvez@loro.com.co>
2020-10-06 20:19:04 -0500

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,7 @@ let urlDB;
 if ( process.env.NODE_ENV === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://admindb:g68YEgekwq65L4gu@cluster0.4ip9r.mongodb.net/cafe'
+    urlDB = process.env.URL
 }
 
 process.env.URLDB = urlDB;
