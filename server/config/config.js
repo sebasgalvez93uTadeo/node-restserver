310f3246c1e1eeb09c20572b1a56bad30f04387f
sebasgalvez-loro <s.galvez@loro.com.co>
2020-10-06 20:04:01 -0500

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,3 +1,22 @@
 // puerto
 
 process.env.PORT = process.env.PORT || 3000;
+
+// =======================
+// Entorno
+// =======================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// =======================
+// Base de datos
+// =======================
+
+let urlDB;
+
+if ( process.env.NODE_ENV === 'dev' ) {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://admindb:g68YEgekwq65L4gu@cluster0.4ip9r.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlDB;
