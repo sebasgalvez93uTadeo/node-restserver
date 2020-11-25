34c6a73317341aa256749edd2fe2661ce6493658
sebasgalvez-loro <s.galvez@loro.com.co>
2020-11-24 19:56:25 -0500

    Login normal

--- server/config/config.js
+++ server/config/config.js
@@ -8,15 +8,26 @@ process.env.PORT = process.env.PORT || 3000;
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
 // =======================
+// Vencimiento del token 
+// =======================
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 *24 * 30;
+
+// =======================
+// SEED de autenticacion
+// =======================
+process.env.SEED = process.env.SEED | 'este-es-el-seed-desarrollo';
+
+// =======================
 // Base de datos
 // =======================
 
 let urlDB;
 
 if ( process.env.NODE_ENV === 'dev' ) {
-    urlDB = 'mongodb://localhost:27017/cafe'
+    urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONGO_URL
+    urlDB = process.env.MONGO_URL;
 }
 
 process.env.URLDB = urlDB;
